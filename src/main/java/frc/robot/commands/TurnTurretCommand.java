f21989aacccd5886a47a6d6da542503bcde1116e
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-01-11 22:15:21 -0500

    Refactor TurretSubsystem to use setMotorSpeed method and add TurnTurretCommand for timed turret rotation

---
+++ src/main/java/frc/robot/commands/TurnTurretCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import frc.robot.turret.TurretSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TurnTurretCommand extends Command {
+    private final TurretSubsystem motorSubsystem;
+    private final Timer timer = new Timer();
+
+    public TurnTurretCommand(TurretSubsystem subsystem) {
+        motorSubsystem = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+        motorSubsystem.setMotorSpeed(-0.5);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        motorSubsystem.stopMotor();
+        timer.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(1.0);
+    }
+}
--- src/main/java/frc/robot/turret/TurretSubsystem.java
+++ src/main/java/frc/robot/turret/TurretSubsystem.java
@@ -1,47 +1,36 @@
 package frc.robot.turret;
 
+// https://claude.ai/chat/420a3d4c-6d25-4de7-8930-f6be5ae112ec
+// Includes more modern example if wanting to persue this further
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.Timer;
 
 public class TurretSubsystem extends SubsystemBase {
     private final WPI_TalonSRX motor;
-    private final Timer timer;
 
     // Constants for motor speed
     private static final double CLOCKWISE_SPEED = 0.5;
     private static final double COUNTERCLOCKWISE_SPEED = -0.5;
+
     int motorCANID = 27;
 
     public TurretSubsystem(int motorCANID) {
         motor = new WPI_TalonSRX(motorCANID);
-        timer = new Timer();
     }
 
-    public void turnCounterClockwiseTimer() {
-        timer.reset();
-        timer.start();
-        while (timer.get() < 1.0) {
-            motor.set(ControlMode.PercentOutput, COUNTERCLOCKWISE_SPEED);
-        }
-        motor.set(ControlMode.PercentOutput, 0);
-        timer.stop();
+    public void setMotorSpeed(double speed) {
+        motor.set(ControlMode.PercentOutput, speed);
     }
 
-    public void turnClockwiseTimer() {
-        timer.reset();
-        timer.start();
-        while (timer.get() < 1.0) {
-            motor.set(ControlMode.PercentOutput, CLOCKWISE_SPEED);
-        }
+    public void stopMotor() {
         motor.set(ControlMode.PercentOutput, 0);
-        timer.stop();
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
     }
+}
 
-} // end of class TurretSubsystem
