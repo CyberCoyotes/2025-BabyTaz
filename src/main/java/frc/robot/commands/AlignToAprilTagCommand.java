b62c2a176c3c2dd382435697e2c73aa4612543c9
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-12-29 23:33:31 -0500

    Refactor alignment commands to use updated VisionSubsystem and remove deprecated classes

--- src/main/java/frc/robot/commands/AlignToAprilTagCommand.java
+++ src/main/java/frc/robot/commands/AlignToAprilTagCommand.java
@@ -8,7 +8,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.subsystems.vision18.VisionSubsystem;
+import frc.robot.subsystems.vision.VisionSubsystem;
+
 import org.littletonrobotics.junction.Logger;
 
 /**
